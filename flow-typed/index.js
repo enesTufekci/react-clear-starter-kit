fdab3ea7ab6b5c0cbc68ee5b4f80c18322d96c69
enesTufekci <enesxtufekci@gmail.com>
2017-09-25 19:02:57 +0200

    flow-typed declerations

---
+++ flow-typed/index.js
@@ -0,0 +1,10 @@
+// eslint-disable
+/*flow-disable*/
+declare module 'react-router-dom' {
+  declare module.exports: any;
+}
+
+declare module 'prop-types' {
+  declare module.exports: any;
+}
+/*flow-enable*/
